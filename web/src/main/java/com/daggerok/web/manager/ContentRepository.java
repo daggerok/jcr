1aae079d70a8d3d4bd4c63943d020c8d56307ecf
Maksim Kostromin <daggerok@gmail.com>
2015-07-12 02:37:11 +0300

    Update ContentRepository.java

--- web/src/main/java/com/daggerok/web/manager/ContentRepository.java
+++ web/src/main/java/com/daggerok/web/manager/ContentRepository.java
@@ -3,7 +3,7 @@ package com.daggerok.web.manager;
 public interface ContentRepository {
     void saveOrUpdate(String filename, byte[] data);
 
-    boolean contains(String filename);
+    boolean exists(String filename);
 
     byte[] read(String filename);
 
